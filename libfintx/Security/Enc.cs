1ad5e775b9ce81e2b33a464a7e1d4742c329ddfe
mrklintscher <torsten.klinger@googlemail.com>
2017-10-24 19:02:17 +0200

    Update: file headers

--- libfintx/Security/Enc.cs
+++ libfintx/Security/Enc.cs
@@ -1,22 +1,22 @@
 ﻿/*	
  * 	
- *  This file is part of hbci4dotnet.
+ *  This file is part of libfintx.
  *  
  *  Copyright (c) 2017 Torsten Klinger
  * 	E-Mail: torsten.klinger@googlemail.com
  * 	
- * 	hbci4dotnet is free software; you can redistribute it and/or
+ * 	libfintx is free software; you can redistribute it and/or
  *	modify it under the terms of the GNU Lesser General Public
  * 	License as published by the Free Software Foundation; either
  * 	version 2.1 of the License, or (at your option) any later version.
  *	
- * 	hbci4dotnet is distributed in the hope that it will be useful,
+ * 	libfintx is distributed in the hope that it will be useful,
  * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
  * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * 	Lesser General Public License for more details.
  *	
  * 	You should have received a copy of the GNU Lesser General Public
- * 	License along with hbci4dotnet; if not, write to the Free Software
+ * 	License along with libfintx; if not, write to the Free Software
  * 	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
  * 	
  */
