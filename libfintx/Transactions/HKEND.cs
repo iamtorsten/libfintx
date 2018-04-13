b9405eeebebc46c53dcb8e489d210d79b90c4c73
mrklintscher <torsten.klinger@googlemail.com>
2018-04-13 10:41:39 +0200

    Add: segment HKEND -> End of dialog

---
+++ libfintx/Transactions/HKEND.cs
@@ -0,0 +1,48 @@
+﻿/*	
+ * 	
+ *  This file is part of libfintx.
+ *  
+ *  Copyright (c) 2016 - 2018 Torsten Klinger
+ * 	E-Mail: torsten.klinger@googlemail.com
+ * 	
+ * 	libfintx is free software; you can redistribute it and/or
+ *	modify it under the terms of the GNU Lesser General Public
+ * 	License as published by the Free Software Foundation; either
+ * 	version 2.1 of the License, or (at your option) any later version.
+ *	
+ * 	libfintx is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * 	Lesser General Public License for more details.
+ *	
+ * 	You should have received a copy of the GNU Lesser General Public
+ * 	License along with libfintx; if not, write to the Free Software
+ * 	Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+ * 	
+ */
+
+using libfintx.Data;
+
+namespace libfintx.Transactions
+{
+    public static class HKEND
+    {
+        /// <summary>
+        /// End of dialog
+        /// </summary>
+        /// <param name="connectionDetails"></param>
+        /// <param name="dialogID"></param>
+        public static string Init_HKEND(ConnectionDetails connectionDetails, string dialogID)
+        {
+            Log.Write("Starting job HKEND: End of dialog");
+
+            string segments = string.Empty;
+
+            segments = "HKEND:" + SEGNUM.SETVal(3) + "1+" + dialogID + "'";
+
+            SEG.NUM = SEGNUM.SETInt(3);
+
+            return FinTSMessage.Send(connectionDetails.Url, FinTSMessage.Create(connectionDetails.HBCIVersion, Segment.HNHBS, Segment.HNHBK, connectionDetails.Blz, connectionDetails.UserId, connectionDetails.Pin, Segment.HISYN, segments, Segment.HIRMS, SEG.NUM));
+        }
+    }
+}
