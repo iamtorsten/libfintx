5d02bc8ca0c2f8bce28cf607b9d474556be7a1d8
Stephan Vedder <vedder@mbits.info>
2019-10-08 16:53:27 +0200

    Work on sample application

---
+++ src/libfintx.Sample/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using libfintx;
+using libfintx.Data;
+
+namespace libfintx.Sample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your banking credentials!");
+            ConnectionDetails details = new ConnectionDetails();
+            details.Account = Console.ReadLine();
+            details.HBCIVersion = 300;
+            HBCIOutput(libfintx.Main.Synchronization(details).Messages);
+        }
+
+        /// <summary>
+        /// HBCI-Nachricht ausgeben
+        /// </summary>
+        /// <param name="hbcimsg"></param>
+        private static void HBCIOutput(IEnumerable<HBCIBankMessage> hbcimsg)
+        {
+            foreach (var msg in hbcimsg)
+            {
+                Console.WriteLine("Code: " + msg.Code + " | " + "Typ: " + msg.Type + " | " + "Nachricht: " + msg.Message);
+            }
+        }
+    }
+}
