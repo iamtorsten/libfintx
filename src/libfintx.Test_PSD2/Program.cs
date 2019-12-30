bb69af4d277b4bef563a60b75971f33519fffab7
Torsten Klinger <torsten.klinger@googlemail.com>
2019-12-30 13:16:41 +0100

    Add PSD2 test project

---
+++ src/libfintx.Test_PSD2/Program.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using libfintx;
+using libfintx.Data;
+
+namespace libfintx.Test_PSD2
+{
+	class Program
+	{
+		static bool Anonymous;
+
+		static void Main(string[] args)
+		{
+			var connectionDetails = new ConnectionDetails()
+			{
+				Account = "xxx",
+				Blz = 76061482,
+				BIC = "GENODEF1HSB",
+				IBAN = "xxx",
+				Url = "https://hbci11.fiducia.de/cgi-bin/hbciservlet",
+				HBCIVersion = 300,
+				UserId = "xxx",
+				Pin = "xxx"
+			};
+
+			Anonymous = false;
+
+			// Sync
+			HBCIOutput(libfintx.Main.Synchronization(connectionDetails).Messages);
+
+			// Transaktionen abfragen
+			var trx = libfintx.Main.Transactions(connectionDetails, new TANDialog(WaitForTAN), Anonymous, new DateTime(2019, 10, 1), DateTime.Now);
+
+			HBCIOutput(trx.Messages);
+
+			if (trx.Data != null) // RawData
+			{
+				foreach (var swift in trx.Data) // SWIFT
+				{
+					if (swift.SWIFTTransactions.Count != 0)
+					{
+						foreach (var swifttrx in swift.SWIFTTransactions) // SWIFT Transaktionen
+						{
+							// Einzelne Transaktion
+							Console.WriteLine(swifttrx.partnerName);
+						}
+					}
+				}
+			}
+
+			Console.ReadLine();
+		}
+
+		/// <summary>
+		/// HBCI-Nachricht ausgeben
+		/// </summary>
+		/// <param name="hbcimsg"></param>
+		private static void HBCIOutput(IEnumerable<HBCIBankMessage> hbcimsg)
+		{
+			foreach (var msg in hbcimsg)
+			{
+				Console.WriteLine("Code: " + msg.Code + " | " + "Typ: " + msg.Type + " | " + "Nachricht: " + msg.Message);
+			}
+		}
+
+		public static string WaitForTAN(TANDialog tanDialog)
+		{
+			foreach (var msg in tanDialog.DialogResult.Messages)
+				Console.WriteLine(msg);
+
+			return Console.ReadLine();
+		}
+	}
+}
