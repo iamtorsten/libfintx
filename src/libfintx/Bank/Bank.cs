abbd1b266035c9284aa5cb21ca7f0e2a55fc0662
Torsten Klinger <torsten@snakx.org>
2021-03-05 10:09:46 +0100

    Parsen der Bankdaten nach Eingabe der Bankleitzahl oder BIC

---
+++ src/libfintx/Bank/Bank.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+
+using libfintx.Model;
+
+namespace libfintx.Bank
+{
+    public class Bank
+    {
+        /// <summary>
+        /// Bankauswahl
+        /// </summary>
+        /// <param name="BLZoderBIC"></param>
+        /// <returns></returns>
+        public MBank Auswahl(String BLZoderBIC)
+        {
+            var lines = File.ReadAllLines("Banken.csv");
+
+            foreach (var line in lines)
+            {
+                if (line.Split(';')[0].Equals(BLZoderBIC) ||
+                    line.Split(';')[1].Equals(BLZoderBIC))
+                {
+                    // BLZ
+                    if (BLZoderBIC.StartsWith("0") ||
+                    BLZoderBIC.StartsWith("1") ||
+                    BLZoderBIC.StartsWith("2") ||
+                    BLZoderBIC.StartsWith("3") ||
+                    BLZoderBIC.StartsWith("4") ||
+                    BLZoderBIC.StartsWith("5") ||
+                    BLZoderBIC.StartsWith("6") ||
+                    BLZoderBIC.StartsWith("7") ||
+                    BLZoderBIC.StartsWith("8") ||
+                    BLZoderBIC.StartsWith("9"))
+                    {
+                        return new MBank() { BLZ = BLZoderBIC, BIC = line.Split(';')[1], BANK = line.Split(';')[2], HBCIVERSION = line.Split(';')[3], URL = line.Split(';')[4] };
+                    }
+                    else // BIC
+                    {
+                        return new MBank() { BLZ = line.Split(';')[0], BIC = BLZoderBIC, BANK = line.Split(';')[2], HBCIVERSION = line.Split(';')[3], URL = line.Split(';')[4] };
+                    }
+                }
+                else
+                    return null;
+            }
+
+            return null;
+        }
+    }
+}
---
+++ src/libfintx/Model/MBank.cs
@@ -0,0 +1,15 @@
+﻿namespace libfintx.Model
+{
+    public class MBank
+    {
+        public string BLZ { get; set; }
+
+        public string BIC { get; set; }
+
+        public string BANK { get; set; }
+
+        public string HBCIVERSION { get; set; }
+
+        public string URL { get; set; }
+    }
+}
