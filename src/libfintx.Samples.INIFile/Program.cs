8e61fd152a87bd1a7b83e9669fc57f2eb5414092
Torsten Klinger <torsten@snakx.org>
2021-03-05 10:02:59 +0100

    Support für INI Dateien hinzugefügt
    Speichern, Auslesen von Einstellungen direkt aus der Bibliothek

---
+++ src/libfintx.Samples.INIFile/Program.cs
@@ -0,0 +1,17 @@
+﻿using libfintx.INIFile;
+
+namespace libfintx.Samples.INIFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var iniFile = new IniFile();
+
+            iniFile.Section("foo").Comment = "This is a test";
+            iniFile.Section("foo").Set("test", "1");
+
+            iniFile.Save("libfintx.ini");
+        }
+    }
+}
