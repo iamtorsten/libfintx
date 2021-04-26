46add02ac3ddb8eb17ca4f7ea3744f51c7c73f78
Torsten Klinger <torsten@snakx.org>
2021-04-26 12:08:29 +0200

    Lizenzänderung
    GNU Lesser General Public v3

--- src/libfintx.Xml/UserSignatureData.cs
+++ src/libfintx.Xml/UserSignatureData.cs
@@ -1,11 +1,24 @@
-﻿/*
- * NetEbics -- .NET Core EBICS Client Library
- * (c) Copyright 2018 Bjoern Kuensting
+﻿/*	
+ * 	
+ *  This file is part of libfintx.
+ *  
+ *  Copyright (C) 2018 Bjoern Kuensting
+ *  
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 3 of the License, or (at your option) any later version.
  *
- * This file is subject to the terms and conditions defined in
- * file 'LICENSE.txt', which is part of this source code package.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ *  Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software Foundation,
+ *  Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ * 	
  */
-
 using System.Xml.Linq;
 
 
