aabaec2d53c05af2cb16c9c13372db2ec09fd4c5
Torsten <thisistorsten@gmail.com>
2022-01-13 12:33:22 +0100

    Update license header

--- src/libfintx.FinTS/Data/BankersOrder.cs
+++ src/libfintx.FinTS/Data/BankersOrder.cs
@@ -2,7 +2,7 @@
  * 	
  *  This file is part of libfintx.
  *  
- *  Copyright (C) 2021 Abid Hussain
+ *  Copyright (C) 2021 - 2002 Abid Hussain
  *  
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public
