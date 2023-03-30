dd6cd461a6ebb20b35654223abf161167e36de1a
Torsten <19864638+iamtorsten@users.noreply.github.com>
2023-03-30 11:08:37 +0200

    Add test case
    https://github.com/iamtorsten/libfintx/issues/113

--- src/libfintx.Tests/HelperTest.cs
+++ src/libfintx.Tests/HelperTest.cs
@@ -102,6 +102,48 @@ HNHBS:7:1+2'".Replace(Environment.NewLine, string.Empty);
         }
 
         [Fact]
+        public void Test_Parse_Message_3()
+        {
+            var message =
+@"HNHBK:1:3+000000001602+300+000006GTTKI00L9SQT0FA5U9RD0U5O+1+000006GTTKI00L9SQT0FA5U9RD0U5O:1'
+HNVSK:998:3+PIN:1+998+1+2::0+1+2:2:13:@8@        :6:1+280:50010517:XXXXXXXXXX:V:0:0+0'
+HNVSD:999:1+@1391@HIRMG:2:2:+3060::Teilweise liegen Warnungen/Hinweise vor.'
+HIRMS:3:2:3+0020::Angemeldet.'
+HIRMS:4:2:4+0020::Informationen fehlerfrei entgegengenommen.+3050::BPD nicht mehr aktuell. Aktuelle Version folgt.+3920::Zugelassene Ein- und Zwei-Schritt-Verfahren für den Benutzer:900'
+HIRMS:5:2:5+0020::Die Synchronisierung der Kundensystem-ID war erfolgreich.'
+HIBPA:6:3:4+7+280:50010517+ING-DiBa+0+1+220:300+200'
+HIKOM:7:4:4+280:50010517+1+3:https?://fints.ing-diba.de/fints/'
+HISPAS:8:1:4+1+1+0+J:J:J:urn?:iso?:std?:iso?:20022?:tech?:xsd?:pain.001.003.03'
+HIPAES:9:1:4+1+1+0'
+HICCSS:10:1:4+1+1+0'
+HITANS:11:1:4+1+1+0+J:N:0:0:900:2:iTAN:iTAN:6:1:Index:3:1:N:N'
+HIKAZS:12:5:4+1+1+390:N:N'
+HICDNS:13:1:4+1+1+1+0:1:360:J:J:J:J:J:N:N:N:J:00:00::0'
+HICSBS:14:1:4+1+1+0+N:N'
+HICSAS:15:1:4+1+1+0+1:360'
+HIWPDS:16:6:4+1+1+0+N:N:N'
+HIWPDS:17:5:4+1+1+N:N:N'
+DIPAES:18:1:4+1+1'
+HICDLS:19:1:4+1+1+1+1:360:N:J'
+HIPROS:20:3:4+1+1'
+HICSES:21:1:4+1+1+0+1:360'
+HICSLS:22:1:4+1+1+0+J'
+HICDBS:23:1:4+1+1+0+N'
+HISALS:24:5:4+1+1'
+HICDES:25:1:4+1+1+1+4:1:360:00:00::0'
+DIPINS:26:1:4+1+1+HKSPA:N:HKPAE:J:HKCCS:J:HKTAN:N:HKKAZ:N:HKCDN:J:HKCSB:N:HKCSA:J:HKWPD:N:DKPAE:J:HKCDL:J:HKPRO:N:HKCSE:J:HKCSL:J:HKCDB:N:HKSAL:N:HKCDE:J'
+HIPINS:27:1:4+1+1+0+5:10:6:Kontonummer::HKSPA:N:HKPAE:J:HKCCS:J:HKTAN:N:HKKAZ:N:HKCDN:J:HKCSB:N:HKCSA:J:HKWPD:N:DKPAE:J:HKCDL:J:HKPRO:N:HKCSE:J:HKCSL:J:HKCDB:N:HKSAL:N:HKCDE:J'
+HISYN:28:4:5+000006GTTKIKNG0ESG1J399FF7DPGI''
+HNHBS:29:1+1''".Replace(Environment.NewLine, string.Empty);
+
+            var client = new FinTsClient(new ConnectionDetails());
+            Helper.Parse_Message(client, message);
+
+            Assert.Equal(3, client.HNHBS);
+            Assert.Equal("900", client.HITAN);
+        }
+
+        [Fact]
         public void Test_Parse_Segments_HITAN_1()
         {
             var message =
